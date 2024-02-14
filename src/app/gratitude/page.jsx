5be7e08dfa10d7023b843557dfc6cb1fccf9ca5d
Savchenko-Andrey <dracxm95@gmail.com>
2024-02-14 17:11:02 +0200

    create page gratitude

---
+++ src/app/gratitude/page.jsx
@@ -0,0 +1,9 @@
+import Gratitude from "@/libs/pages/gratitude/gratitude";
+
+export default function Home() {
+  return (
+    <>
+      <Gratitude />
+    </>
+  );
+}
---
+++ src/libs/pages/gratitude/gratitude.jsx
@@ -0,0 +1,9 @@
+import styles from "./gratitude.module.scss"
+
+export default function Gratitude() {
+  return (
+    <section className={styles.section}>
+      <div>dsffssdfsfs</div>
+    </section>
+  )
+}
